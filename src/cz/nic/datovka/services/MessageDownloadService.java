651b2c78e1b9059665b2097464c7d7cec59dacd6
Martin Strbacka <m.strbacka@gmail.com>
2012-11-01 08:32:07 +0100

    Download only signed messages (not parsed yet)

--- src/cz/nic/datovka/services/MessageDownloadService.java
+++ src/cz/nic/datovka/services/MessageDownloadService.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 import android.app.IntentService;
 import android.content.ContentUris;
@@ -16,11 +15,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.ResultReceiver;
 import android.widget.Toast;
-import cz.abclinuxu.datoveschranky.common.entities.Attachment;
 import cz.abclinuxu.datoveschranky.common.entities.MessageEnvelope;
-import cz.abclinuxu.datoveschranky.common.entities.content.FileContent;
-import cz.abclinuxu.datoveschranky.common.interfaces.AttachmentStorer;
-import cz.abclinuxu.datoveschranky.common.interfaces.DataBoxDownloadService;
 import cz.nic.datovka.R;
 import cz.nic.datovka.connector.Connector;
 import cz.nic.datovka.connector.DatabaseHelper;
@@ -30,15 +25,15 @@ import cz.nic.datovka.contentProviders.ReceivedMessagesContentProvider;
 import cz.nic.datovka.contentProviders.SentMessagesContentProvider;
 import cz.nic.datovka.tinyDB.exceptions.HttpException;
 
-public class MessageDownloadService extends IntentService implements AttachmentStorer{
+public class MessageDownloadService extends IntentService {
 	public static final int UPDATE_PROGRESS = 8344;
 	public static final String MSG_ID = "msgid";
 	public static final String FOLDER = "folder";
 	public static final String RECEIVER = "receiver";
-	
+
 	private static String PROGRAM_FOLDER;
 	private static final int INBOX = 0;
-	
+
 	private String directory;
 	private long messageId;
 	private int messageIsdsId;
@@ -50,49 +45,57 @@ public class MessageDownloadService extends IntentService implements AttachmentS
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		PROGRAM_FOLDER = Environment.getExternalStorageDirectory().getPath() + "/Datovka";
+		PROGRAM_FOLDER = Environment.getExternalStorageDirectory().getPath()
+				+ "/Datovka";
 		messageId = intent.getLongExtra(MSG_ID, 0);
 		folder = intent.getIntExtra(FOLDER, 0);
-		ResultReceiver receiver = (ResultReceiver) intent.getParcelableExtra(RECEIVER);
-		
-		// Get ISDS ID and MSGBOX ID of the message 
+		ResultReceiver receiver = (ResultReceiver) intent
+				.getParcelableExtra(RECEIVER);
+
+		// Get ISDS ID and MSGBOX ID of the message
 		Uri singleUri;
 		String[] projection;
 		String IsdsIdColName;
 		String msgBoxIdColName;
-		if(folder == INBOX){
-			singleUri = ContentUris.withAppendedId(ReceivedMessagesContentProvider.CONTENT_URI,messageId);
-			projection = new String[]{DatabaseHelper.RECEIVED_MESSAGE_ISDS_ID, DatabaseHelper.RECEIVED_MESSAGE_MSGBOX_ID};
+		if (folder == INBOX) {
+			singleUri = ContentUris.withAppendedId(
+					ReceivedMessagesContentProvider.CONTENT_URI, messageId);
+			projection = new String[] {
+					DatabaseHelper.RECEIVED_MESSAGE_ISDS_ID,
+					DatabaseHelper.RECEIVED_MESSAGE_MSGBOX_ID };
 			IsdsIdColName = DatabaseHelper.RECEIVED_MESSAGE_ISDS_ID;
 			msgBoxIdColName = DatabaseHelper.RECEIVED_MESSAGE_MSGBOX_ID;
-		}
-		else{
-			singleUri = ContentUris.withAppendedId(SentMessagesContentProvider.CONTENT_URI,messageId);
-			projection = new String[]{DatabaseHelper.SENT_MESSAGE_ISDS_ID, DatabaseHelper.SENT_MESSAGE_MSGBOX_ID};
+		} else {
+			singleUri = ContentUris.withAppendedId(
+					SentMessagesContentProvider.CONTENT_URI, messageId);
+			projection = new String[] { DatabaseHelper.SENT_MESSAGE_ISDS_ID,
+					DatabaseHelper.SENT_MESSAGE_MSGBOX_ID };
 			IsdsIdColName = DatabaseHelper.SENT_MESSAGE_ISDS_ID;
 			msgBoxIdColName = DatabaseHelper.SENT_MESSAGE_MSGBOX_ID;
 		}
-		Cursor msgCursor = getContentResolver().query(singleUri, projection, null, null, null);
+		Cursor msgCursor = getContentResolver().query(singleUri, projection,
+				null, null, null);
 		msgCursor.moveToFirst();
-		
+
 		int isdsIdColIndex = msgCursor.getColumnIndex(IsdsIdColName);
 		int msgBoxIdColIndex = msgCursor.getColumnIndex(msgBoxIdColName);
-		
+
 		messageIsdsId = msgCursor.getInt(isdsIdColIndex);
 		int msgBoxId = msgCursor.getInt(msgBoxIdColIndex);
 		msgCursor.close();
-		
+
 		// Connect to WS
 		if (!Connector.isOnline()) {
 			connectToWs(msgBoxId);
 		}
-		
+
 		// If the download folder not exists create it
 		checkExternalStorage();
-		directory = PROGRAM_FOLDER + "/" + Integer.toString(messageIsdsId) + "_" + Long.toString(messageId) + "/";
+		directory = PROGRAM_FOLDER + "/" + Integer.toString(messageIsdsId)
+				+ "_" + Long.toString(messageId) + "/";
 		File destFolder = new File(directory);
-		if(!destFolder.exists()){
-			if(!destFolder.mkdirs()){
+		if (!destFolder.exists()) {
+			if (!destFolder.mkdirs()) {
 				// TODO
 				try {
 					throw new Exception("neco se po..");
@@ -102,19 +105,19 @@ public class MessageDownloadService extends IntentService implements AttachmentS
 				}
 			}
 		}
-		
-		//TODO get rid of the MessageEnvelope
+
+		// TODO get rid of the MessageEnvelope
 		MessageEnvelope envelope = new MessageEnvelope();
 		envelope.setMessageID(Integer.toString(messageIsdsId));
-		
+
 		// uložíme celou podepsanou zprávu
 		FileOutputStream fos = null;
 		try {
-			fos = new FileOutputStream(new File(destFolder, messageIsdsId + ".bin"));
-			if(folder == INBOX){
+			fos = new FileOutputStream(new File(destFolder, messageIsdsId
+					+ ".bin"));
+			if (folder == INBOX) {
 				Connector.downloadSignedReceivedMessage(envelope, fos);
-			}
-			else{
+			} else {
 				Connector.downloadSignedSentMessage(envelope, fos);
 			}
 			fos.flush();
@@ -122,95 +125,81 @@ public class MessageDownloadService extends IntentService implements AttachmentS
 			insertAttachmentToDb(directory + messageIsdsId + ".bin",
 					getResources().getString(R.string.signed_message_name),
 					"application/pkcs7+xml");
-			
-			// stáhneme přílohy k dosle zprávě
-			if(folder == INBOX){
-				Connector.downloadMessage(envelope, this);
-				//Hash hash = Connector.verifyMessage(envelope);
-			}
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (HttpException e) {
-			//TODO
+			// TODO
 			String errorMessage = e.getMessage();
-			Toast.makeText(getApplicationContext(), "chyba " + errorMessage, Toast.LENGTH_LONG).show();
+			Toast.makeText(getApplicationContext(), "chyba " + errorMessage,
+					Toast.LENGTH_LONG).show();
 			this.stopSelf();
 		}
-		
-		
-		
+
 		/*
-		Iterator<Attachment> messageIterator = attachments.iterator();
-		while(messageIterator.hasNext()){
-			Attachment attachment = messageIterator.next();
-			insertAttachmentToDb(attachment.get, filename)
-		}
-		*/
+		 * Iterator<Attachment> messageIterator = attachments.iterator();
+		 * while(messageIterator.hasNext()){ Attachment attachment =
+		 * messageIterator.next(); insertAttachmentToDb(attachment.get,
+		 * filename) }
+		 */
 		/*
-		String urlToDownload = "http://download.documentfoundation.org/libreoffice/stable/3.6.2/rpm/x86_64/LibO_3.6.2_Linux_x86-64_install-rpm_en-US.tar.gz";
-		try {
-			URL url = new URL(urlToDownload);
-			URLConnection connection = url.openConnection();
-			connection.connect();
-			// this will be useful so that you can show a typical 0-100%
-			// progress bar
-			int fileLength = connection.getContentLength();
-
-			// download the file
-			
-			InputStream input = new BufferedInputStream(url.openStream());
-			OutputStream output = new FileOutputStream(PROGRAM_FOLDER + "pokus");
-			
-
-			byte data[] = new byte[1024];
-			long total = 0;
-			int count;
-			while ((count = input.read(data)) != -1) {
-				total += count;
-				// publishing the progress....
-				Bundle resultData = new Bundle();
-				resultData.putInt("progress", (int) (total * 100 / fileLength));
-				receiver.send(UPDATE_PROGRESS, resultData);
-				output.write(data, 0, count);
-			}
-
-			output.flush();
-			output.close();
-			input.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-*/
+		 * String urlToDownload =
+		 * "http://download.documentfoundation.org/libreoffice/stable/3.6.2/rpm/x86_64/LibO_3.6.2_Linux_x86-64_install-rpm_en-US.tar.gz"
+		 * ; try { URL url = new URL(urlToDownload); URLConnection connection =
+		 * url.openConnection(); connection.connect(); // this will be useful so
+		 * that you can show a typical 0-100% // progress bar int fileLength =
+		 * connection.getContentLength();
+		 * 
+		 * // download the file
+		 * 
+		 * InputStream input = new BufferedInputStream(url.openStream());
+		 * OutputStream output = new FileOutputStream(PROGRAM_FOLDER + "pokus");
+		 * 
+		 * 
+		 * byte data[] = new byte[1024]; long total = 0; int count; while
+		 * ((count = input.read(data)) != -1) { total += count; // publishing
+		 * the progress.... Bundle resultData = new Bundle();
+		 * resultData.putInt("progress", (int) (total * 100 / fileLength));
+		 * receiver.send(UPDATE_PROGRESS, resultData); output.write(data, 0,
+		 * count); }
+		 * 
+		 * output.flush(); output.close(); input.close(); } catch (IOException
+		 * e) { e.printStackTrace(); }
+		 */
 		Bundle resultData = new Bundle();
 		resultData.putInt("progress", 100);
 		receiver.send(UPDATE_PROGRESS, resultData);
 	}
 
 	private void connectToWs(int msgBoxId) {
-		Uri msgBoxUri = ContentUris.withAppendedId(MsgBoxContentProvider.CONTENT_URI,msgBoxId);
-		String[] msgBoxProjection = new String[] {
-				DatabaseHelper.MSGBOX_LOGIN,
-				DatabaseHelper.MSGBOX_PASSWORD,
-				DatabaseHelper.MSGBOX_TEST_ENV };
-		Cursor msgBoxCursor = getContentResolver().query(msgBoxUri, msgBoxProjection, null, null, null);
+		Uri msgBoxUri = ContentUris.withAppendedId(
+				MsgBoxContentProvider.CONTENT_URI, msgBoxId);
+		String[] msgBoxProjection = new String[] { DatabaseHelper.MSGBOX_LOGIN,
+				DatabaseHelper.MSGBOX_PASSWORD, DatabaseHelper.MSGBOX_TEST_ENV };
+		Cursor msgBoxCursor = getContentResolver().query(msgBoxUri,
+				msgBoxProjection, null, null, null);
 		msgBoxCursor.moveToFirst();
-		
-		int loginIndex = msgBoxCursor.getColumnIndex(DatabaseHelper.MSGBOX_LOGIN);
-		int passwordIndex = msgBoxCursor.getColumnIndex(DatabaseHelper.MSGBOX_PASSWORD);
-		int envIndex = msgBoxCursor.getColumnIndex(DatabaseHelper.MSGBOX_TEST_ENV);
+
+		int loginIndex = msgBoxCursor
+				.getColumnIndex(DatabaseHelper.MSGBOX_LOGIN);
+		int passwordIndex = msgBoxCursor
+				.getColumnIndex(DatabaseHelper.MSGBOX_PASSWORD);
+		int envIndex = msgBoxCursor
+				.getColumnIndex(DatabaseHelper.MSGBOX_TEST_ENV);
 		String login = msgBoxCursor.getString(loginIndex);
 		String password = msgBoxCursor.getString(passwordIndex);
 		int environment = msgBoxCursor.getInt(envIndex);
 		msgBoxCursor.close();
 		try {
-			Connector.connect(login, password, environment, getApplicationContext());
+			Connector.connect(login, password, environment,
+					getApplicationContext());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	private void checkExternalStorage() {
@@ -247,30 +236,14 @@ public class MessageDownloadService extends IntentService implements AttachmentS
 		}
 	}
 
-	public OutputStream store(MessageEnvelope envelope, Attachment attachment)
-			throws IOException {
-		String name = name(envelope, attachment);
-		insertAttachmentToDb(directory + name, name, attachment.getMimeType());
-		
-		File output = new File(directory, name);
-		attachment.setContents(new FileContent(output));
-		return new FileOutputStream(output);
-	}
-	
-	protected String name(MessageEnvelope envelope, Attachment attachment) {
-        String prefix = envelope.getMessageID();
-        String description = attachment.getDescription();
-
-        return prefix + "_" + description;
-    }
-	
-	private void insertAttachmentToDb(String path, String filename, String mime){
+	private void insertAttachmentToDb(String path, String filename, String mime) {
 		ContentValues value = new ContentValues();
 		value.put(DatabaseHelper.ATTACHMENTS_MSG_ID, messageId);
 		value.put(DatabaseHelper.ATTACHMENTS_MSG_FOLDER_ID, folder);
 		value.put(DatabaseHelper.ATTACHMENTS_PATH, path);
 		value.put(DatabaseHelper.ATTACHMENTS_FILENAME, filename);
 		value.put(DatabaseHelper.ATTACHMENTS_MIME, mime);
-		getContentResolver().insert(AttachmentsContentProvider.CONTENT_URI, value);
+		getContentResolver().insert(AttachmentsContentProvider.CONTENT_URI,
+				value);
 	}
 }
