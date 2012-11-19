18ab0123276ca2f6132766d970351ca0c4c04f52
Martin Strbacka <m.strbacka@gmail.com>
2012-11-19 09:41:56 +0100

    Download progress dialog now survives screen rotation and handles service stopping correctly.

--- src/cz/nic/datovka/fragments/MessageDownloadProgressFragment.java
+++ src/cz/nic/datovka/fragments/MessageDownloadProgressFragment.java
@@ -10,50 +10,57 @@ import android.os.ResultReceiver;
 import android.support.v4.app.DialogFragment;
 import android.widget.Toast;
 import cz.nic.datovka.R;
+import cz.nic.datovka.activities.Application;
 import cz.nic.datovka.services.MessageDownloadService;
 
 public class MessageDownloadProgressFragment extends DialogFragment {
 	private static ProgressDialog mProgressDialog;
-	
+	private static Handler handler;
+
 	private static final String MSG_ID = "msgid";
 	private static final String FOLDER = "folder";
-	
-	public static MessageDownloadProgressFragment newInstance(long messageId, int folder){
+	private static boolean runService = true;
+
+	public static MessageDownloadProgressFragment newInstance(long messageId, int folder) {
 		MessageDownloadProgressFragment mdpf = new MessageDownloadProgressFragment();
 		Bundle bundle = new Bundle();
 		bundle.putInt(FOLDER, folder);
 		bundle.putLong(MSG_ID, messageId);
-		
+
 		mdpf.setArguments(bundle);
 		return mdpf;
 	}
-	
+
 	public Dialog onCreateDialog(Bundle SavedInstanceState) {
 		long messageId = getArguments().getLong(MSG_ID);
 		int folder = getArguments().getInt(FOLDER);
-		
+
 		mProgressDialog = new ProgressDialog(getActivity());
 		mProgressDialog.setMessage(getResources().getString(R.string.download_attachment_progress));
 		mProgressDialog.setIndeterminate(false);
 		mProgressDialog.setMax(100);
 		mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-		
-		Intent intent = new Intent(getActivity(), MessageDownloadService.class);
-		intent.putExtra(MessageDownloadService.FOLDER, folder);
-		intent.putExtra(MessageDownloadService.MSG_ID, messageId);
-		intent.putExtra(MessageDownloadService.RECEIVER, new DownloadReceiver(new Handler()));
-		getActivity().startService(intent);
-		
+
+		if (runService) {
+			runService = false;
+			Intent intent = new Intent(getActivity(), MessageDownloadService.class);
+			intent.putExtra(MessageDownloadService.FOLDER, folder);
+			intent.putExtra(MessageDownloadService.MSG_ID, messageId);
+			handler = new Handler();
+			intent.putExtra(MessageDownloadService.RECEIVER, new DownloadReceiver(handler));
+			getActivity().startService(intent);
+		}
+
 		return mProgressDialog;
 	}
-	
+		
 	@Override
-	public void onDismiss(DialogInterface dialog) {
-		super.onDismiss(dialog);
+	public void onCancel(DialogInterface dialog) {
+		System.out.println("cancel");
 		getActivity().stopService(new Intent(getActivity(), MessageDownloadService.class));
 	}
-	
-	private class DownloadReceiver extends ResultReceiver {
+
+	private static class DownloadReceiver extends ResultReceiver {
 		public DownloadReceiver(Handler handler) {
 			super(handler);
 		}
@@ -67,11 +74,10 @@ public class MessageDownloadProgressFragment extends DialogFragment {
 				if (progress == 100) {
 					mProgressDialog.dismiss();
 				}
-			}
-			else if(resultCode == MessageDownloadService.ERROR){
+			} else if (resultCode == MessageDownloadService.ERROR) {
 				String msg = resultData.getString("error");
 				mProgressDialog.dismiss();
-				Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
+				Toast.makeText(Application.ctx, msg, Toast.LENGTH_LONG).show();
 			}
 		}
 	}
